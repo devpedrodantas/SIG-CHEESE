210f212726aa45fb73ad04f861d5ad7b4661048b
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-02 01:04:54 -0300

    Adiciona #include "validacao.h" em validacao.c

--- validacao.c
+++ validacao.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>  // Para a função isalpha
+#include "validacao.h"
+
 
 //---------------------------------------------------------
 //                   Validação para Letra
