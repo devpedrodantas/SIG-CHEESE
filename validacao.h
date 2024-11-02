868ae58dcddcb10d6e1d9d7da7699ab8db67d18e
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-01 23:26:18 -0300

    Cria o arquivo validacao.h

---
+++ validacao.h
@@ -0,0 +1,9 @@
+// Assinaturas das funções de validação
+
+int validaNome(const char* nome);
+int validaCPF(const char* cpf);
+int validaFone(const char* fone);
+int validaEmail(const char* email);
+int validaLetra(char c);
+int validaDigito(char c);
+int validaData(const char* data);
