8f8a5f3c10b9576d9acd9a7e2230d59fc08bc327
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-15 18:16:56 -0300

    Atualiza definição de leNome para alocação dinâmica
    -  Definição de TAM_NOME foi removida;
    - Altera o tipo do parâmetro de nome[] para char **nome em leNome para suportar alocação dinâmica;

--- entradas.h
+++ entradas.h
@@ -1,12 +1,10 @@
-//Tamanhos máximos para as variáveis de entrada
-#define TAM_NOME 50
+// Assinaturas das funções de entradas
 #define TAM_CPF 12
 #define TAM_EMAIL 50
 #define TAM_DATA 11
 #define TAM_FONE 12
 
-// Declaração das funções auxiliares de entrada
-void leNome(char nome []);
+void leNome(char **nome); // Tipo do parâmetro 'nome' alterado para ponteiro
 void leCpf(char cpf[]);
 void leEmail(char email[]);
 void leData(char data[]);
