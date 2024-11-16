47f7127b9e16acb150ad97ef28d71d42c82e3600
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-16 02:07:59 -0300

    Refatora Funções de Leitura para Usar Ponteiros
    Substitui char[] por char ** nas funções, permitindo o uso das funções genéricas para entradas de diferentes tamanhos. O CPF continuou com alocação estática.

--- entradas.h
+++ entradas.h
@@ -1,11 +1,9 @@
 // Assinaturas das funções de entradas
-#define TAM_CPF 12
-#define TAM_EMAIL 50
-#define TAM_DATA 11
-#define TAM_FONE 12
 
-void leNome(char **nome); // Tipo do parâmetro 'nome' alterado para ponteiro
+#define TAM_CPF 12
+
+void leNome(char **nome);   // Tipo do parâmetro 'nome' alterado para ponteiro
 void leCpf(char cpf[]);
-void leEmail(char email[]);
-void leData(char data[]);
-void leFone(char fone[]);
+void leEmail(char **email); // Tipo do parâmetro 'email' alterado para ponteiro
+void leData(char **data);   // Tipo do parâmetro 'data' alterado para ponteiro
+void leFone(char **fone);   // Tipo do parâmetro 'fone' alterado para ponteiro
