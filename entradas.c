526db7b6b96c278c700e55a11c9a38824b703f0e
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-16 01:06:49 -0300

    Adiciona Função Genérica para Entradas Longas
    Cria a função leEntradaMax para ler entradas de até 255 caracteres e alocar memória dinamicamente. Usada para entradas como nome e e-mail.

--- entradas.c
+++ entradas.c
@@ -3,6 +3,23 @@
 #include <string.h>  // Inclusão da biblioteca para manipulação de strings.
 #include "entradas.h"
 
+// Função para ler entradas longas (ex.: nome, e-mail) e alocar memória dinamicamente
+void leEntradaMax(char **entrada) {
+    char buffer[256];
+    int tam;
+
+    // Lê a entrada do usuário
+    scanf("%255[^\n]", buffer);
+    getchar();
+
+    // Aloca memória para a entrada com base no tamanho lido
+    tam = strlen(buffer);
+    *entrada = (char*) malloc(tam + 1);
+
+    // Copia a string para a memória alocada
+    strcpy(*entrada, buffer);
+}
+
 void leNome(char **nome) {
     char entrada[101];  // Buffer temporário para entrada inicial
     int tam;
