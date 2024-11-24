cc1274a0ee71c5cfbe509b7d9de9c65aa85a72f5
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-24 00:37:18 -0300

    Adiciona proteção contra múltiplas inclusões no cliente.h

--- cliente.h
+++ cliente.h
@@ -1,5 +1,11 @@
+#ifndef CLIENTE_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
+#define CLIENTE_H  // Marca CLIENTE_H como definido
+
+// Declaração das funções relacionadas ao cliente
 void menu_cliente(void);
 void cadastra_cliente(void);
 void pesquisa_cliente(void);
 void atualiza_cliente(void);
 void exclui_cliente(void);
+
+#endif // Finaliza a proteção
