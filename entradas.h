a0e5644f6f995f6b0cffc3e4af1a546216c863af
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-25 20:37:21 -0300

    Separa Funções de Entrada para Cliente e Funcionario no Cabeçalho
    - Refatora funções genéricas de entrada para separar Cliente e Funcionario;
    
    - Adiciona assinaturas específicas para Funcionario;
    
    - Inclui o cabeçalho funcionario.h;
    
    - Implementa guardas de inclusão (#ifndef, #define) nos cabeçalhos para evitar múltiplas inclusões e melhorar a organização do código.

--- entradas.h
+++ entradas.h
@@ -1,18 +1,29 @@
-// Assinaturas das funções de entradas
+#ifndef ENTRADAS_H  // Proteção contra múltiplas inclusões
+#define ENTRADAS_H
 
 #define TAM_CPF 12
 #define TAM_CODIGO 7
 
-#include "cliente.h"  // Inclui a definição do tipo Cliente
+#include "cliente.h"      // Inclui a definição do tipo Cliente
+#include "funcionario.h"  // Inclui a definição do tipo Funcionario
 
-// Funções que agora recebem um ponteiro para a struct Cliente
-void leNome(Cliente *cliente); 
-void leCpf(Cliente *cliente);
-void leEmail(Cliente *cliente); 
-void leData(Cliente *cliente);  
-void leFone(Cliente *cliente); 
+// Funções que recebem um ponteiro para a struct Cliente
+void leNomeCliente(Cliente *cliente);
+void leCpfCliente(Cliente *cliente);
+void leEmailCliente(Cliente *cliente);
+void leDataCliente(Cliente *cliente);
+void leFoneCliente(Cliente *cliente);
 
+// Funções que recebem um ponteiro para a struct Funcionario
+void leNomeFuncionario(Funcionario *funcionario);
+void leCpfFuncionario(Funcionario *funcionario);
+void leEmailFuncionario(Funcionario *funcionario);
+void leDataFuncionario(Funcionario *funcionario);
+void leFoneFuncionario(Funcionario *funcionario);
+
+// Funções auxiliares
 void leCodigo(char codigo[TAM_CODIGO]);
 void leComposicao(char **comp);
 void leTipoLeite(char **tipo);
 
+#endif // ENTRADAS_H
