a3e4632194eecc5af276dbe364546e4645a6f3a1
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-25 20:22:30 -0300

    Adiciona Estrutura Funcionario no funcionario.h
    Inclui o typedef struct Funcionario no arquivo funcionario.h para armazenar os dados relacionados ao funcionário

--- funcionario.h
+++ funcionario.h
@@ -1,5 +1,20 @@
+#ifndef FUNCIONARIO_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
+#define FUNCIONARIO_H  // Marca FUNCIONARIO_H como definido
+
+// Declaração das funções relacionadas ao funcionário
 void menu_funcionario(void);
 void cadastra_funcionario(void);
 void pesquisa_funcionario(void);
 void atualiza_funcionario(void);
 void exclui_funcionario(void);
+
+// Estrutura para armazenar os dados do funcionário
+typedef struct {
+    char *nome;       // Nome dinâmico
+    char cpf[12];     // CPF com tamanho fixo
+    char *email;      // E-mail dinâmico
+    char *data;       // Data de nascimento ou cadastro dinâmico
+    char *fone;       // Telefone dinâmico
+} Funcionario;
+
+#endif // Finaliza a proteção
