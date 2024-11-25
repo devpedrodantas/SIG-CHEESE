e3c983e01ca29dcf1388ffabeb8486870c7dd29c
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-25 02:09:51 -0300

    Atualiza Assinaturas das Funções para uso da Struct Cliente

--- entradas.h
+++ entradas.h
@@ -3,12 +3,14 @@
 #define TAM_CPF 12
 #define TAM_CODIGO 7
 
-void leNome(char **nome);   // Tipo do parâmetro 'nome' alterado para ponteiro
-void leCpf(char cpf[TAM_CPF]);
-void leEmail(char **email); // Tipo do parâmetro 'email' alterado para ponteiro
-void leData(char **data);   // Tipo do parâmetro 'data' alterado para ponteiro
-void leFone(char **fone);   // Tipo do parâmetro 'fone' alterado para ponteiro
+// Funções que agora recebem um ponteiro para a struct Cliente
+void leNome(Cliente *cliente); 
+void leCpf(Cliente *cliente);
+void leEmail(Cliente *cliente); 
+void leData(Cliente *cliente);  
+void leFone(Cliente *cliente); 
 
 void leCodigo(char codigo[TAM_CODIGO]);
 void leComposicao(char **comp);
 void leTipoLeite(char **tipo);
+
