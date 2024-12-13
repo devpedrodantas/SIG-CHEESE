b5777e3555dac9e761a7504c579aa379aa1ff171
IvysonWNM <ivysonnunes@hotmail.com>
2024-12-13 17:15:26 -0300

    Cria Funções de leApenas e Refatora as Funções de Leitura do Cliente
    - Adiciona o include de validacao.h;
    
    - Cria funções que fazem apenas a leitura das entradas como (leApenasNome, leApenasCpf, leApenasEmail...)
    
    - Refatora as funções de leitura do cliente: Reorganiza e simplifica a estrutura do código, aprimorando a clareza e a manutenção;

--- entradas.c
+++ entradas.c
@@ -1,39 +1,142 @@
 #include <stdio.h>
 #include <stdlib.h>           // Inclusão da biblioteca para manipulação de memória dinâmica.
 #include <string.h>           // Inclusão da biblioteca para manipulação de strings.
-#include "cliente.h"          // Inclui a definição do tipo Cliente
-#include "funcionario.h"      // Inclui a definição do tipo Funcionário
-#include "queijo.h"           // Inclui a definição do tipo Queijo
+#include "cliente.h"          // Define a estrutura e funções do Cliente.
+#include "funcionario.h"      // Define a estrutura e funções do Funcionário.
+#include "queijo.h"           // Define a estrutura e funções do Queijo.
+#include "validacao.h"        // Contém funções para validações gerais.
 
 
 // Funções específicas para Cliente               (Ainda é necessário evitar esta redundância com cliente, funcionário e queijo)
-void leNomeCliente(Cliente *cliente) {
+
+///--------------------------------------------------------///
+///                        NOME                            ///
+//---------------------------------------------------------///
+// Função para capturar apenas o nome do cliente
+void leApenasNome(Cliente *cliente) {
     fgets(cliente->nome, sizeof(cliente->nome), stdin);              
     cliente->nome[strcspn(cliente->nome, "\n")] = '\0';
 }
 
-void leCpfCliente(Cliente *cliente) {
+// Função para ler e validar o nome do cliente
+void leNomeCliente(Cliente *cliente) {
+    printf("|-> Nome do cliente: ");
+    do {
+       leApenasNome(cliente);
+        if (validaNome(cliente->nome)) {
+            printf("Nome válido\n");
+            break;
+        } else {
+            printf("Nome inválido, tente novamente apertando a tecla ENTER");
+            getchar();
+            printf("|-> Nome do cliente: ");
+        }
+    } while (!validaNome(cliente->nome));
+}
+
+
+///--------------------------------------------------------///
+///                         CPF                            ///
+//---------------------------------------------------------///
+// Função para capturar apenas o CPF do cliente
+void leApenasCpf(Cliente *cliente) {
     fgets(cliente->cpf, sizeof(cliente->cpf), stdin);
     cliente->cpf[strcspn(cliente->cpf, "\n")] = '\0';
 }
 
-void leEmailCliente(Cliente *cliente) {
+// Função para ler e validar o CPF do cliente
+void leCpfCliente(Cliente *cliente) {
+    printf("|-> CPF (somente números): ");
+    do {
+        leApenasCpf(cliente);
+        if (validaCPF(cliente->cpf)) {
+            printf("CPF válido\n");
+            break;
+        } else {
+            printf("CPF inválido, tente novamente apertando a tecla ENTER");
+            getchar();
+            printf("|-> CPF (somente números): ");
+        }
+    } while (!validaCPF(cliente->cpf)); // Continua até o CPF ser válido
+}
+
+
+///--------------------------------------------------------///
+///                        EMAIL                           ///
+//---------------------------------------------------------///
+// Função para capturar apenas o email do cliente
+void leApenasEmail(Cliente *cliente) {
     fgets(cliente->email, sizeof(cliente->email), stdin);
     cliente->email[strcspn(cliente->email, "\n")] = '\0';
 }
 
-void leDataCliente(Cliente *cliente) {
+// Função para ler e validar o Email do cliente
+void leEmailCliente(Cliente *cliente) {
+    printf("|-> Email: ");
+    do {
+        leApenasEmail(cliente);    // Lê o email dinamicamente através do ponteiro
+        if (validaEmail(cliente->email)) {
+            printf("Email válido\n");
+            break;
+        } else {
+            printf("Email inválido, tente novamente apertando a tecla ENTER");
+            getchar();
+            printf("|-> Email: ");
+        }
+    } while(!validaEmail(cliente->email));
+}
+
+
+///--------------------------------------------------------///
+///                        DATA                            ///
+//---------------------------------------------------------///
+// Função para capturar apenas a data do cliente
+void leApenaData(Cliente *cliente) {
     fgets(cliente->data, sizeof(cliente->data), stdin);
     cliente->data[strcspn(cliente->data, "\n")] = '\0';
 }
 
+// Função para ler e validar a data do cliente
+void leDataCliente(Cliente *cliente) {
+    printf("|-> Data de nascimento (DD/MM/AAAA): ");
+        do {
+        leApenaData(cliente);   // Lê a data dinamicamente através do ponteiro
+        if (validaData(cliente->data)) {
+            printf("Data válida\n");
+            break;
+        } else {
+            printf("Data inválida, tente novamente apertando a tecla ENTER");
+            getchar();
+            printf("|-> Data de nascimento (DD/MM/AAAA): ");
+        }
+    } while(!validaData(cliente->data));
+}
 
-void leFoneCliente(Cliente *cliente) {
+
+///--------------------------------------------------------///
+///                        FONE                            ///
+//---------------------------------------------------------///
+// Função para capturar apenas o número de telefone do cliente
+void leApenasFone(Cliente *cliente) {
     fgets(cliente->fone, sizeof(cliente->fone), stdin);
     cliente->fone[strcspn(cliente->fone, "\n")] = '\0';
 }
 
-
+// Função para ler e validar o  número de telefone do cliente
+void leFoneCliente(Cliente *cliente) {
+  printf("|-> Telefone (somente números): ");
+     do {
+        leApenasFone(cliente);    // Lê o número de telefone dinamicamente através do ponteiro
+        if (validaFone(cliente->fone)) {
+            printf("Número de telefone válido\n");
+            break;
+        } else {
+            printf("Número de telefone inválido, tente novamente apertando a tecla ENTER");
+            getchar();
+            printf("|-> Telefone (somente números): ");
+        }
+    } while(!validaFone(cliente->fone));
+}
 // Função geral para leitura de entradas
 void leEntrada(char **entrada, size_t tamanho) {
     char buffer[tamanho];
