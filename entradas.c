027aafb685e67a2d63b6532016a038e75093ea24
IvysonWNM <ivysonnunes@hotmail.com>
2024-12-16 18:51:37 -0300

    Desconsidera as Funções que não Estão Sendo Usadas

--- entradas.c
+++ entradas.c
@@ -432,82 +432,84 @@ void leTipoLeite(Queijo *queijo) {
 
 
 
-////////////////////////////////////////////////
-// Função geral para leitura de entradas
-void leEntrada(char **entrada, size_t tamanho) {
-    char buffer[tamanho];
-    fgets(buffer, tamanho, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0';  // Remove o \n no final da string
+///////////////////////////////////////////////////
+//            Funções não mais usadas            
+//
+// // Função geral para leitura de entradas
+// void leEntrada(char **entrada, size_t tamanho) {
+//     char buffer[tamanho];
+//     fgets(buffer, tamanho, stdin);
+//     buffer[strcspn(buffer, "\n")] = '\0';  // Remove o \n no final da string
 
-    if (*entrada != NULL) {
-        free(*entrada);  // Libera memória previamente alocada
-    }
+//     if (*entrada != NULL) {
+//         free(*entrada);  // Libera memória previamente alocada
+//     }
 
-    *entrada = (char*) malloc(strlen(buffer) + 1);
-    if (*entrada == NULL) {
-        perror("Erro ao alocar memória");
-        exit(1);  // Sai caso a alocação falhe
-    }
+//     *entrada = (char*) malloc(strlen(buffer) + 1);
+//     if (*entrada == NULL) {
+//         perror("Erro ao alocar memória");
+//         exit(1);  // Sai caso a alocação falhe
+//     }
 
-    strcpy(*entrada, buffer);  // Copia o buffer para o ponteiro
-}
-
-
-// Funções temporárias para ler entradas curtase longas (serão REMOVIDAS depois)
-void leEntradaMax(char **entrada) {
-    leEntrada(entrada, 256);
-}
-
-void leEntradaMin(char **entrada) {
-    leEntrada(entrada, 16);
-}
+//     strcpy(*entrada, buffer);  // Copia o buffer para o ponteiro
+// }
 
 
-// Funções para Funcionario
-void leNomeFuncionario(Funcionario *funcionario) {
-  leEntradaMax(&funcionario->nome);  // Passa o ponteiro para o nome
-}
+// // Funções temporárias para ler entradas curtase longas (serão REMOVIDAS depois)
+// void leEntradaMax(char **entrada) {
+//     leEntrada(entrada, 256);
+// }
 
-void leCpfFuncionario(Funcionario *funcionario) {
-    scanf("%11s", funcionario->cpf);  // Acessa diretamente funcionario->cpf
-    getchar();
-}
-
-void leEmailFuncionario(Funcionario *funcionario) {
-    leEntradaMax(&funcionario->email);  // Passa o ponteiro para o email
-}
-
-void leDataFuncionario(Funcionario *funcionario) {
-    leEntradaMin(&funcionario->data);  // Passa o ponteiro para a data
-}
-
-void leFoneFuncionario(Funcionario *funcionario) {
-    leEntradaMin(&funcionario->fone);  // Passa o ponteiro para o telefone
-}
-
-// Funções para Queijo
-void leNomeQueijo(Queijo *queijo) {
-  leEntradaMax(&queijo->nome);  // Passa o ponteiro para o nome
-}
-
-void leDataFabricacao(Queijo *queijo) {
-    leEntradaMin(&queijo->data_fabricacao);  // Passa o ponteiro para a data
-}
-
-void leDataValidade(Queijo *queijo) {
-    leEntradaMin(&queijo->data_validade);  // Passa o ponteiro para a data
-}
+// void leEntradaMin(char **entrada) {
+//     leEntrada(entrada, 16);
+// }
 
 
-void leCodigo(Queijo *queijo) {
-    scanf("%6s", queijo->codigo); //Acessa diretamente queijo->codigo
-    getchar();
-}
+// // Funções para Funcionario
+// void leNomeFuncionario(Funcionario *funcionario) {
+//   leEntradaMax(&funcionario->nome);  // Passa o ponteiro para o nome
+// }
 
-void leComposicao(Queijo *queijo) {
-    leEntradaMax(&queijo->comp); // Passa o ponteiro para a composição/ingrediente
-}
+// void leCpfFuncionario(Funcionario *funcionario) {
+//     scanf("%11s", funcionario->cpf);  // Acessa diretamente funcionario->cpf
+//     getchar();
+// }
 
-void leTipoLeite(Queijo *queijo) {
-    leEntradaMin(&queijo->tipo); // Passa o ponteiro para a tipo
-}
+// void leEmailFuncionario(Funcionario *funcionario) {
+//     leEntradaMax(&funcionario->email);  // Passa o ponteiro para o email
+// }
+
+// void leDataFuncionario(Funcionario *funcionario) {
+//     leEntradaMin(&funcionario->data);  // Passa o ponteiro para a data
+// }
+
+// void leFoneFuncionario(Funcionario *funcionario) {
+//     leEntradaMin(&funcionario->fone);  // Passa o ponteiro para o telefone
+// }
+
+// // Funções para Queijo
+// void leNomeQueijo(Queijo *queijo) {
+//   leEntradaMax(&queijo->nome);  // Passa o ponteiro para o nome
+// }
+
+// void leDataFabricacao(Queijo *queijo) {
+//     leEntradaMin(&queijo->data_fabricacao);  // Passa o ponteiro para a data
+// }
+
+// void leDataValidade(Queijo *queijo) {
+//     leEntradaMin(&queijo->data_validade);  // Passa o ponteiro para a data
+// }
+
+
+// void leCodigo(Queijo *queijo) {
+//     scanf("%6s", queijo->codigo); //Acessa diretamente queijo->codigo
+//     getchar();
+// }
+
+// void leComposicao(Queijo *queijo) {
+//     leEntradaMax(&queijo->comp); // Passa o ponteiro para a composição/ingrediente
+// }
+
+// void leTipoLeite(Queijo *queijo) {
+//     leEntradaMin(&queijo->tipo); // Passa o ponteiro para a tipo
+// }
