ece88bdc7a82eda69f447f1a4ae735fab1630c65
IvysonWNM <ivysonnunes@hotmail.com>
2024-12-16 14:31:48 -0300

    Coloca Arrays Fixos na Estrutura do Cliente
    - Declara a função busca_cliente

--- cliente.h
+++ cliente.h
@@ -1,21 +1,25 @@
 #ifndef CLIENTE_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
 #define CLIENTE_H  // Marca CLIENTE_H como definido
 
-// Declaração das funções relacionadas ao cliente
-void menu_cliente(void);
-void cadastra_cliente(void);
-void pesquisa_cliente(void);
-void atualiza_cliente(void);
-void exclui_cliente(void);
-
 // Estrutura para armazenar os dados do cliente
 typedef struct {
     char status;      // Status do cliente (ativo, inativo, etc.)
-    char *nome;       // Nome dinâmico
-    char cpf[12];     // CPF com tamanho fixo
-    char *email;      // E-mail dinâmico
-    char *data;       // Data de nascimento ou cadastro dinâmico
-    char *fone;       // Telefone dinâmico
+    char nome[52];       // Nome dinâmico
+    char cpf[13];     // CPF com tamanho fixo
+    char email[258];      // E-mail dinâmico
+    char data[15];       // Data de nascimento ou cadastro dinâmico
+    char fone[15];       // Telefone dinâmico
 } Cliente;
+
+
+// Declaração das funções relacionadas ao cliente
+void menu_cliente(void);
+void cadastra_cliente(void);
+
+void pesquisa_cliente(void);
+void busca_cliente (const char *cpf_busca);
+
+void atualiza_cliente(void);
+void exclui_cliente(void);
 
 #endif // Finaliza a proteção
