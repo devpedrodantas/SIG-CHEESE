a07deb777f7072bd54ddfe5f8ec354bd625c1678
IvysonWNM <ivysonnunes@hotmail.com>
2024-12-16 20:17:21 -0300

    Refatora a Estrutura Funcionario e Declara a Função que Verifica CPF

--- funcionario.h
+++ funcionario.h
@@ -1,6 +1,16 @@
 #ifndef FUNCIONARIO_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
 #define FUNCIONARIO_H  // Marca FUNCIONARIO_H como definido
 
+// Estrutura para armazenar os dados do funcionário
+typedef struct {
+    char status;              // Status do funcionário (ativo, inativo, etc)
+    char nome[52];            // Nome como array estático
+    char cpf[13];             // CPF com tamanho como array estático
+    char email[258];          // E-mail como array estático
+    char data[15];            // Data de nascimento como array estático
+    char fone[15];            // Telefone como array estático
+} Funcionario;
+
 // Declaração das funções relacionadas ao funcionário
 void menu_funcionario(void);
 void cadastra_funcionario(void);
@@ -8,13 +18,7 @@ void pesquisa_funcionario(void);
 void atualiza_funcionario(void);
 void exclui_funcionario(void);
 
-// Estrutura para armazenar os dados do funcionário
-typedef struct {
-    char *nome;       // Nome dinâmico
-    char cpf[12];     // CPF com tamanho fixo
-    char *email;      // E-mail dinâmico
-    char *data;       // Data de nascimento ou cadastro dinâmico
-    char *fone;       // Telefone dinâmico
-} Funcionario;
+int verificaCpfCadastradoF(const char *cpf);
+
 
 #endif // Finaliza a proteção
