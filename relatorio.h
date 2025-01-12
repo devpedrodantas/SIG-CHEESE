258b6d6943f80961d15cc2391e7f12841db8c22a
IvysonWNM <ivysonnunes@hotmail.com>
2025-01-12 20:13:32 -0300

    Cria o arquivo relatorio.h

---
+++ relatorio.h
@@ -0,0 +1,10 @@
+#ifndef RELATORIO_H
+#define RELATORIO_H 
+
+// Declaração das funções relacionadas aos relatórios
+void menu_relatorio(void);         // Exibe o menu principal de relatórios
+void relatorio_cliente(void);      // Exibe o relatório de clientes
+void relatorio_funcionario(void);  // Exibe o relatório de funcionários
+void relatorio_queijo(void);       // Exibe o relatório de queijos
+
+#endif 
