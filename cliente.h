20c2c01554fe13b41004cbc4866dc9374b7da51b
IvysonWNM <ivysonnunes@hotmail.com>
2025-01-12 20:39:02 -0300

    Incorpora a struct Endereco à struct Cliente
    . Adiciona a struct Endereco à struct Cliente
    
    . Inclui o cabeçalho estruturas.h

--- cliente.h
+++ cliente.h
@@ -1,5 +1,6 @@
 #ifndef CLIENTE_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
 #define CLIENTE_H  // Marca CLIENTE_H como definido
+#include "estruturas.h"  // Inclui a definição da estrutura Endereco
 
 // Estrutura para armazenar os dados do cliente
 typedef struct {
@@ -9,6 +10,7 @@ typedef struct {
     char email[258];      // E-mail como array estático
     char data[15];        // Data de nascimento como array estático
     char fone[15];        // Telefone como array estático
+    Endereco endereco;    // Endereço associado ao cliente (definido em estruturas.h)
 } Cliente;
 
 
