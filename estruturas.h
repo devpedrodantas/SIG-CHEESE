8302cd26e317aaefd9afba2b06102d444812b494
IvysonWNM <ivysonnunes@hotmail.com>
2025-01-12 20:17:22 -0300

    Cria o arquivo estruturas.h
    . A função desse arquivo é para armazenar estruturas genéricas que serão usadas em mais de um módulo

---
+++ estruturas.h
@@ -0,0 +1,11 @@
+#ifndef ESTRUTURAS_H  
+#define ESTRUTURAS_H  
+
+// Definição da struct Endereco
+typedef struct {
+    char bairro[30];
+    char cidade[30];
+    char estado[25];
+} Endereco;
+
+#endif
