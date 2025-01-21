33d2d4e5f4f838c4ad8a962d31b15e5cffd92248
IvysonWNM <ivysonnunes@hotmail.com>
2025-01-21 15:28:44 -0300

    Declara as novas funções do relatório de cliente por compras
    Declara as funções busca_cliente_por_compras, buscaVendas e leCpfRelatorio

--- relatorio.h
+++ relatorio.h
@@ -1,19 +1,28 @@
 #ifndef RELATORIO_H
 #define RELATORIO_H 
 
-// Declaração das funções relacionadas aos relatórios
+// ========================= RELATÓRIOS GERAIS =========================
 void menu_relatorio(void);         // Exibe o menu principal de relatórios
 void relatorio_cliente(void);      // Exibe o relatório de clientes
 void relatorio_funcionario(void);  // Exibe o relatório de funcionários
 void relatorio_queijo(void);       // Exibe o relatório de queijos
 
+
+// ================= RELATÓRIOS DE CLIENTES POR BAIRRO =================
 void lista_clientes_por_bairro(void);
 void leBairroRelatorio(char* bairro_lido);
 void buscaBairroRelatorio(char* bairro_lido); 
 
+
+// ==================== RELATÓRIOS QUEIJOS POR TIPO ====================
 void busca_tipo_queijo_Relatorio(char* tipo_lido); 
 void leTipoRelatorio(char* tipo_lido); 
 void lista_queijo_tipo(void);
 
+
+// ================= RELATÓRIOS DE CLIENTES POR VENDAS =================
+void busca_cliente_por_compras(void);
+void buscaVendas(const char *cpf_relatorio);
+void leCpfRelatorio(char *cpf);
 
 #endif 
